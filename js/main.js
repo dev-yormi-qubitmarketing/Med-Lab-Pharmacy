e9117d30127fea176e06840bf0b141133c99beb3
Yormi Altamiranda <yormian@gmail.com>
2018-09-07 09:56:43 -0400

    Portotipo 3 beta 2

--- js/main.js
+++ js/main.js
@@ -5,9 +5,11 @@ jQuery(document).ready(function($) {
     if ($(this).scrollTop() > 100) {
       $('.back-to-top').fadeIn('slow');
       $('#header').addClass('header-fixed');
+      $('.info').addClass('oculto');
     } else {
       $('.back-to-top').fadeOut('slow');
       $('#header').removeClass('header-fixed');
+      $('.info').removeClass('oculto');
     }
   });
 
